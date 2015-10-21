e50a1671ed4abb3dfbe87145ff9493b6b537108e
pwhiting <pwhiting@MININT-8N3ELIQ.uncfsu.edu>
2015-10-21 13:48:13 -0400

    uses functions

--- src/main/java/HyphenationRules.java
+++ src/main/java/HyphenationRules.java
@@ -1,6 +1,7 @@
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,10 +10,37 @@ public class HyphenationRules {
 	private static final String VOWELS = "([aeiouy])";
 	private static final String CONSONANTS = "(qu|tr|br|str|st|sl|bl|cr|ph|ch|[bcdfghjklmnpqrstvwxz])";
 
-	private static final Pattern RULE_ONE = Pattern.compile(VOWELS + CONSONANTS + CONSONANTS + VOWELS,
-			Pattern.CASE_INSENSITIVE);
-	private static final Pattern RULE_TWO = Pattern.compile(VOWELS + CONSONANTS + VOWELS, Pattern.CASE_INSENSITIVE);
-	private static final Pattern E_FIX = Pattern.compile("(-)" + CONSONANTS + "(e$)", Pattern.CASE_INSENSITIVE); 
+	private static final Pattern RULE_ONE_REGEX = Pattern.compile(VOWELS + CONSONANTS + CONSONANTS + VOWELS, Pattern.CASE_INSENSITIVE);
+	private static final Function<String, String> RULE_ONE = new Function<String, String>() {
+
+		@Override
+		public String apply(String arg0) {
+			return arg0.replaceAll(RULE_ONE_REGEX.pattern(), "$1$2-$3$4");
+		}
+		
+	};
+	
+	private static final Pattern RULE_TWO_REGEX = Pattern.compile(VOWELS + CONSONANTS + VOWELS, Pattern.CASE_INSENSITIVE);
+	private static final Function<String, String> RULE_TWO = new Function<String, String>() {
+
+		@Override
+		public String apply(String t) {
+			return t.replaceAll(RULE_TWO_REGEX.pattern(), "$1-$2$3");
+		}
+		
+	};
+
+	private static final Pattern E_FIX_REGEX = Pattern.compile("(-)" + CONSONANTS + "(e$)", Pattern.CASE_INSENSITIVE);
+	private static final Function<String, String> E_FIX = new Function<String, String>() {
+
+		@Override
+		public String apply(String t) {
+			return t.replaceAll(E_FIX_REGEX.pattern(), "$2$3");
+		}
+		
+	};
+	
+	private static final Function<String, String> PROCEDURE = RULE_TWO.andThen(RULE_ONE).andThen(RULE_TWO).andThen(E_FIX).andThen(RULE_ONE);
 
 	public static void main(String[] args) {
 
@@ -29,11 +57,7 @@ public class HyphenationRules {
 			String[] words = input.toString().split(" ");
 			
 			for (String word : words) {
-				String s1 = word.replaceAll(RULE_TWO.pattern(), "$1-$2$3");
-				String s2 = s1.replaceAll(RULE_ONE.pattern(), "$1$2-$3$4");
-				s2 = s2.replaceAll(RULE_TWO.pattern(), "$1-$2$3").replaceAll(E_FIX.pattern(), "$2$3");
-				s2 = s2.replaceAll(RULE_ONE.pattern(), "$1$2-$3$4");
-				System.out.println(s2);
+				System.out.println(PROCEDURE.apply(word));
 			}
 			
 		}
